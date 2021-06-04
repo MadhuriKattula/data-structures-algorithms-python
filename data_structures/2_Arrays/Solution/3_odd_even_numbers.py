4b5875ccc3cb65b465f62ab7760e620a4227a1d1
MadhuriKattula <84833904+MadhuriKattula@users.noreply.github.com>
2021-06-04 12:25:32 +0530

    Update 3_odd_even_numbers.py

--- data_structures/2_Arrays/Solution/3_odd_even_numbers.py
+++ data_structures/2_Arrays/Solution/3_odd_even_numbers.py
@@ -1,9 +1,5 @@
-max = int(input("Enter max number: "))
-
-odd_numbers = []
-
-for i in range(max):
-    if i%2 == 1:
-        odd_numbers.append(i)
-
-print("Odd numbers: ",odd_numbers)
+n=int(input("enter the number upto which you would like to get the odd numbers"))
+odd_numbers=[]
+for i in range(n//2+1):
+    odd_numbers.append(2*i+1)
+print("required odd numbers : ",odd_numbers)
